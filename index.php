e547f7076e07aa4ac1d22a6ffa5ae4dda70fcb1a
Emuekpere, Prince Obosizake <princeobosizake@gmail.com>
2021-09-15 16:36:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en" style="background-image: url('images/Splash2.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>CABMAN</title>
+    <style>
+        @media screen and (max-width: 750px) {
+            .Main {
+                max-width: 30% !important;
+            }
+
+            body {
+                background: transparent;
+            }
+        }
+        
+    </style>
+</head>
+
+<body>
+
+    <!--Loading Container Start-->
+    <div id="load" class="loading-overlay display-flex flex-column justify-content-center align-items-center">
+        <div class="primary-color font-28 fas fa-spinner fa-spin"></div>
+    </div>
+    <!--Loading Container End-->
+
+    <!-- Choose Appropriate Application To View Start-->
+    <div class="all-wide-container w-100 h-100">
+        <div style="margin-top: 46%;" class="d-flex justify-content-center align-items-center flex-column margin-bottom-up">
+            <!--<img style="max-width: 300px;" src="images\Cabman-logo.png" class="margin-bottom-30" alt="Main Logo">-->
+            <a style="background: green; color: white!important;" href="customer/includes/accountcheck.php" target="" class="btn btn-transparent-primary text-white text-uppercase w-100 mb-3">Rider</a>
+            <a style="background: green; color: white!important;" href="driver/includes/accountcheck.php" target="" class="btn btn-transparent-primary text-white text-uppercase w-100">Driver</a>
+            <a style="margin-top: 91%;" href="https://ipaynow247.com/paybill_utility.php" class="btn btn-center width-100 display-block btn-primary text-uppercase margin-top-10">Pay Bills/Utility</a>
+        </div>
+    </div>
+    <!-- Choose Appropriate Application To View Start-->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.4.1.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
